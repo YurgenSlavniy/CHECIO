1b1f765105635c8475f704e3d56bc08307b8a7fb
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2022-06-03 10:59:21 +0300

    Update 01_Initiation.py
    add solution 7

--- 01_Initiation.py
+++ 01_Initiation.py
@@ -459,16 +459,76 @@ def backward_string(val: str) -> str:
 # ___________________________________________________________________________________
 # SOLUTION 7. <>
 
+# Your optional code here
+# You can import some modules or create additional functions
+def checkio(number: int) -> str:
+    if number % 5 == 0 and number % 3 == 0:
+        return "Fizz Buzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return str(number)
+# Some hints:
+# Convert a number in the string with str(n)
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio(15))
+    
+    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert checkio(6) == "Fizz", "6 is divisible by 3"
+    assert checkio(5) == "Buzz", "5 is divisible by 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
                
-# <><><><><>   <><><><><>               
-               
-# ___________________________________________________________________________________
+# <><><><><> Best "Creative" Solution  <><><><><>               
+checkio=lambda n:("Fizz "*(1-n%3)+"Buzz "*(1-n%5))[:-1]or str(n)
 
-# ___________________________________________________________________________________
-# ___________________________________________________________________________________
-# ___________________________________________________________________________________
-# ___________________________________________________________________________________
-# ___________________________________________________________________________________
+# <><><><><> Best "Speedy" Solution <><><><><>  
+def checkio(number):
+    if number % 3 == 0:
+        result = 'Fizz'
+        if number % 5 == 0:
+            result += ' Buzz'
+    elif number % 5 == 0:
+        result = 'Buzz'
+    else:
+        result = s
+    return result
+
+# <><><><><> Best "3rd party" Solution <><><><><> 
+import numpy as np
+
+def checkio(a):
+    if (np.mod(a,5)==0)&(np.mod(a,3)==0):
+        n="Fizz Buzz"
+        print('lol')
+    elif np.mod(a,3)==0:
+        n="Fizz"
+    elif np.mod(a,5)==0:
+        n="Buzz"
+    else:
+        n=str(a)
+    return n
+
+# <><><><><> Best "Scary" Solution <><><><><> 
+ghostbuster = str
+
+def checkio(number):
+    scary_number = 1 * number
+    
+    woo = not scary_number % 5
+    boo = not scary_number % 3
+    
+    if woo and boo:
+        return "Fizz Buzz"
+    if woo:
+        return "Buzz"
+    if boo:
+        return "Fizz"
+    return ghostbuster(scary_number)
+
 # ___________________________________________________________________________________
 # MISSION 8. 
 #  ><   
@@ -477,6 +537,26 @@ def backward_string(val: str) -> str:
 # ___________________________________________________________________________________
 # ___________________________________________________________________________________
 # SOLUTION 8. <>
+
+               
+# <><><><><>   <><><><><>   
+
+
+# ___________________________________________________________________________________
+
+# ___________________________________________________________________________________
+# ___________________________________________________________________________________
+# ___________________________________________________________________________________
+# ___________________________________________________________________________________
+# ___________________________________________________________________________________
+# ___________________________________________________________________________________
+# MISSION 9. 
+#  ><   
+# How many zeros are at the end?
+# Elementary <> 
+# ___________________________________________________________________________________
+# ___________________________________________________________________________________
+# SOLUTION 9. <>
 
                
 # <><><><><>   <><><><><>               
