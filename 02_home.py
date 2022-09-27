d95839edc9e88f980c978562e7022c9b072bb028
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2022-09-27 14:52:00 +0300

    Update 02_home.py
    add solution 11

--- 02_home.py
+++ 02_home.py
@@ -831,6 +831,27 @@ def remove_min_max(data: set, total:int) -> set:
 
 # ___________________________________________________________________________________
 # SOLUTION 11. <>
+from typing import Tuple
+
+def sum_by_types(items: list[str, int]) -> tuple[str, int] | list[str, int]:
+    ints = 0
+    strs = ''
+    for item in items:
+        if type(item) is int:
+            ints += item
+        else:
+            strs += item
+    return (strs, ints)
+
+print("Example:")
+print(list(sum_by_types([])))
+
+assert list(sum_by_types([])) == ["", 0]
+assert list(sum_by_types([1, 2, 3])) == ["", 6]
+assert list(sum_by_types(["1", 2, 3])) == ["1", 5]
+assert list(sum_by_types(["1", "2", 3])) == ["12", 3]
+assert list(sum_by_types(["1", "2", "3"])) == ["123", 0]
+assert list(sum_by_types(["size", 12, "in", 45, 0])) == ["sizein", 57]
 
 # <><><><><>  <><><><><>
 # ___________________________________________________________________________________
