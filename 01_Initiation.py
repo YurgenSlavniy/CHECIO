e9a84156491b6059331ae1f042b3bad1f4fee046
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2022-12-13 09:07:46 +0300

    add solution

--- 01_Initiation.py
+++ 01_Initiation.py
@@ -1316,3 +1316,28 @@ def checkio(data: list) -> list:
 # assert checkio(7) == "7"
 # ___________________________________________________________________________________
 # SOLUTION 18. <>
+def checkio(num: int) -> str:
+    if num % 3 == 0:
+        return "Fizz"
+    else:
+        return str(num)
+
+
+print("Example:")
+print(checkio(3))
+
+# These "asserts" are used for self-checking
+assert checkio(15) == "Fizz"
+assert checkio(6) == "Fizz"
+assert checkio(10) == "10"
+assert checkio(7) == "7"
+# ___________________________________________________________________________________
+# MISSION 19. 
+#  ><   
+#  ?
+#   <> 
+#  --
+# ___________________________________________________________________________________
+
+# ___________________________________________________________________________________
+# SOLUTION 19. <>
