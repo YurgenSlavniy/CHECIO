44f540e8acb74f56697830c787f3ed5e543a0e0e
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-01-20 15:53:17 +0300

    Update 01_Initiation.py

--- 01_Initiation.py
+++ 01_Initiation.py
@@ -1598,11 +1598,32 @@ def is_acceptable_password(password: str) -> bool:
 
 # ___________________________________________________________________________________
 # MISSION 22. 
-# ><   
-# ?
-#  <> 
-# --
+# Acceptable Password IV ><   
+# Verify password by conditions ?
+# Elementary+ <> 
+# bool series string --
 # ___________________________________________________________________________________
+# Elementary+
+# English UK
+
+# In this mission you need to create a password verification function.
+# The verification conditions are:
+
+# - the length should be bigger than 6;
+# - should contain at least one digit, but it cannot consist of just digits;
+# - if the password is longer than 9 - previous rule (about one digit), is not required.
+
+# Input: A string.
+# Output: A bool.
+
+# Examples:
+# assert is_acceptable_password("short") == False
+# assert is_acceptable_password("short54") == True
+# assert is_acceptable_password("muchlonger") == True
+# assert is_acceptable_password("ashort") == False
+
+# How it’s used: For password verification form. Also it's good to learn how the task can be evaluated.
+
 # SOLUTION 22. <> 
 
 # <><><><><> Best "Clear" Solution <><><><><>
