f034470371be55e2d8a4b01464a169b3d0df8a3c
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-01-21 21:23:20 +0300

    solution 22

--- 01_Initiation.py
+++ 01_Initiation.py
@@ -1625,6 +1625,33 @@ def is_acceptable_password(password: str) -> bool:
 # How it’s used: For password verification form. Also it's good to learn how the task can be evaluated.
 
 # SOLUTION 22. <> 
+# Taken from mission Acceptable Password III
+def is_acceptable_password(password: str) -> bool:
+    if len(password) > 9:
+        return True
+    
+    if len(password) > 6:
+        if password.isdigit():
+            return False
+        else:
+            c2 = any(map(str.isdigit, password))
+            return c2
+    else:
+        return False
+
+
+assert is_acceptable_password("short") == False
+assert is_acceptable_password("6789543") == False
+assert is_acceptable_password("short54") == True
+assert is_acceptable_password("muchlonger") == True
+assert is_acceptable_password("ashort") == False
+assert is_acceptable_password("notshort") == False
+assert is_acceptable_password("muchlonger5") == True
+assert is_acceptable_password("sh5") == False
+assert is_acceptable_password("1234567") == False
+assert is_acceptable_password("12345678910") == True
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative " Solution <><><><><>
