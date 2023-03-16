aee72b5d0396989f409f5665e0c5d2ce584a85cc
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-03-16 09:52:14 +0300

    Update Three_Words.py

--- missions/Three_Words.py
+++ missions/Three_Words.py
@@ -23,7 +23,7 @@
 # Предусловия: Исходная строка содержит только слова и/или числа. Смешанных слов нет (перемешанные цифры и буквы).
 # 0 < len(words) < 100
 # ___________________________________________________________________________________
-# SOLUTION <> 
+# SOLUTION 24. <> 
 def checkio(words: str) -> bool:
     count = 0
     for word in words.split():
@@ -33,6 +33,48 @@ def checkio(words: str) -> bool:
     return count >= 3
 
 # <><><><><> Best "Clear" Solution <><><><><>
+def checkio(words):
+    succ = 0
+    for word in words.split():
+        succ = (succ + 1)*word.isalpha()
+        if succ == 3: return True
+    else: return False
+    
 # <><><><><> Best "Creative " Solution <><><><><>
+checkio=lambda x:"www" in "".join('w' if w.isalpha() else 'd' for w in x.split())
+
+checkio=lambda x:"www" in "".join('dw'[w.isalpha()] for w in x.split())
 # <><><><><> Best "Speedy" Solution <><><><><>
+import re
+
+def checkio(words):
+    return True if re.search('\D+\s\D+\s\D+', words) else False
+
+# <><><><><> Best "3rd party" Solution <><><><><>
+import numpy as np
+import re
+
+def checkio(words: str) -> bool:
+    
+    # find the index of words
+    # use np.diff and re to check succession
+    index_gap = np.diff([i for i,w in enumerate(words.split(' ')) if w.isalpha()])
+    
+    index_str = ''.join([str(j) for j in index_gap])
+    
+    pattern = re.compile(r'11')
+    
+    return len(re.findall(pattern, index_str))>0
+
+
 # <><><><><> Best "Uncategorized" Solution <><><><><>
+def checkio(words: str) -> bool:
+    count: int = 0
+    for word in words.split():
+        if word[0].isalpha():
+            count += 1
+            if count >= 3:
+                return True
+        else:
+            count = 0
+    return False
