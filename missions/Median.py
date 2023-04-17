0e76c4ba93353849f90a296632dc1493a44f8395
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-04-17 16:04:57 +0300

    Update Median.py

--- missions/Median.py
+++ missions/Median.py
@@ -54,3 +54,72 @@ assert checkio([33, 56, 62]) == 56
 assert checkio([21, 56, 84, 82, 52, 9]) == 54
 assert checkio([100, 1, 1, 1, 1, 1, 1]) == 1
 assert checkio([64, 6, 92, 7, 70, 5]) == 35.5
+
+# <><><><><> Best "Clear" Solution <><><><><>
+def checkio(data):
+    data.sort()
+    half = len(data) // 2
+    return (data[half] + data[~half]) / 2
+
+# <><><><><> Best "Creative" Solution <><><><><>
+checkio=lambda d:(lambda t,n:t[n]+t[-n-1])(sorted(d),len(d)//2)/2
+
+# <><><><><> Best "Speedy" Solution <><><><><>
+# Find the k'th-smallest value in a, worst case O(n)
+# Skip the first d elements and consider only the next n
+def select(a, d, n, k):
+    if n <= 5:
+        return sorted(a[d:d+n])[k]
+
+    # Find median of medians of 5
+    medians = [sorted(a[i:i+5])[2] for i in range(d, d+n-4, 5)]
+    m = len(medians)
+    median = select(medians, 0, m, m // 2)
+
+    # Partition around the median.
+    # a[d:i]     <= median
+    # a[j+1:d+n] >= median
+    i, j = d, d+n-1
+    while i <= j:
+        while a[i] < median: i += 1
+        while a[j] > median: j -= 1
+        if i <= j:
+            a[i], a[j] = a[j], a[i]
+            i += 1
+            j -= 1
+
+    if d+k < i: return select(a, d, i-d, k)
+    else:       return select(a, i, n+d-i, k+d-i)
+
+def checkio(data):
+    n = len(data)
+    if n % 2 == 1:
+        return select(data, 0, n, n//2)
+    else:
+        return 0.5 * (select(data, 0, n, n//2-1) + select(data, 0, n, n//2))
+
+# <><><><><> Best "3rd party" Solution <><><><><>
+from typing import List
+import numpy as np
+
+def checkio(data: List[int]) -> [int, float]:
+    return np.median(data)
+
+# <><><><><> Uncategorized <><><><><>
+def checkio(data: list[int]) -> int | float:
+    # replace this for solution
+    m = 0
+    m1 = 0
+    m2 = 0
+    data = sorted(data)
+    #data.sort()
+    if len(data) % 2 == 0:
+        m1 = data[len(data) // 2 -1]
+        m2 = data[len(data) // 2]
+        m = (m1 + m2) /2
+    else:
+        m = data[len(data) // 2]
+        
+    return m
+
+# ___________________________________________________________________________________
