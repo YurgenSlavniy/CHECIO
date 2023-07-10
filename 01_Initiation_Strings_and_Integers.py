2c71dca5d49efe4c7bed44b078d997b386068c5a
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-07-10 16:08:49 +0300

    Update 01_Initiation_Strings_and_Integers.py

--- 01_Initiation_Strings_and_Integers.py
+++ 01_Initiation_Strings_and_Integers.py
@@ -2469,10 +2469,64 @@ assert translate("sooooso aaaaaaaaa") == "sos aaa"
 print("The mission is done! Click 'Check Solution' to earn rewards!")
 
 # <><><><><> Best "Clear" Solution <><><><><>
-# <><><><><> Best "Creative " Solution <><><><><>
-# <><><><><> Best "Speedy" Solution <><><><><>
-# <><><><><> Best "Uncategorized" Solution <><><><><>
+VOWELS = "aeiouy"
 
+def translate(phrase):
+    output = ""
+    c = 0
+    
+    while c < len(phrase):
+        output += phrase[c]
+        if phrase[c] in VOWELS:
+            c = c + 3
+        elif phrase[c] == ' ':
+            c = c + 1
+        else:
+            c = c + 2
+
+    return output
+
+# <><><><><> Best "Creative " Solution <><><><><>
+import re,functools
+translate=functools.partial(re.sub,r"(\w)(\1\1|.)",r"\1")
+
+# <><><><><> Best "Speedy" Solution <><><><><>
+translate=lambda s:s and s[0]+translate(s[1+(s[0]!=' ')+(s[0]in'aeiouy'):])
+
+# <><><><><> Best "3rd party" Solution <><><><><>
+VOWELS = "aeiouy"
+
+def translate(phrase):
+    phrase = list(phrase)
+    # print(phrase)
+    for x in range(len(phrase)):
+        try:
+            if phrase[x] not in VOWELS and phrase[x+1] in VOWELS and phrase[x] != ' ':
+                phrase.pop(x+1)
+        except IndexError:
+            continue    
+    phrase = ''.join(phrase)
+    
+    for x in range(len(phrase)):
+        try:
+            if phrase[x] in VOWELS and phrase[x] == phrase[x+1] and phrase[x] == phrase[x+2]:
+                phrase = phrase.replace(phrase[x]*3, phrase[x])
+        except IndexError:
+            continue
+    return phrase
+
+# <><><><><> Best "Uncategorized" Solution <><><><><>
+def translate(text: str) -> str:
+    for i in "aeiouy":
+        text = text.replace(i+i+i, i)
+    new_text = text[0]
+    for i in range(1, len(text)):
+        if text[i - 1] not in " aeiouy":
+            new_text += ''
+        else:
+            new_text += text[i]
+            
+    return new_text
 
 # ___________________________________________________________________________________
 # MISSION 29. 
