3cc096665deaa044ab8cfa7c23820945cf5b687c
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-07-18 09:32:38 +0300

    Update 04_Strings_Theory.py

--- 04_Strings_Theory.py
+++ 04_Strings_Theory.py
@@ -317,6 +317,20 @@ assert is_number("a5") == False
 # Precondition: The text contains only letters, digits and whitespace.
 # ___________________________________________________________________________________
 # SOLUTION 5. <>
+def is_number(val: str) -> bool:
+    return val.isdigit()
+
+
+print("Example:")
+print(is_number("34"))
+
+# These "asserts" are used for self-checking
+assert is_number("34") == True
+assert is_number("df") == False
+assert is_number("") == False
+assert is_number("a5") == False
+assert is_number("ITS A NUMBER") == False
+assert is_number("5a") == False
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
@@ -327,11 +341,78 @@ assert is_number("a5") == False
 
 # ___________________________________________________________________________________
 # MISSION 6. 
-#  >< 
-#  ? 
-#  <>
-#  --
+# Text Formatting >< 
+# Отформатируйте текст в соответствии с заданным стилем ? 
+# Elementary+ <>
+# Russian text --
 # ___________________________________________________________________________________
+# Elementary+
+# EN FR PL Russian UK ZH-HANS
+
+# Вам предоставлена длинная строка (набранная моноширинным шрифтом), 
+# и вы должны разбить ее, чтобы соблюсти заданную ширину. 
+# После этого вам нужно отформатировать текст в соответствии с заданным стилем: 
+# - "l" означает, что вы должны выровнять текст по левому краю - left, 
+# - "c" - по центру - center, 
+# - "r" - по правому краю - right, 
+# - а "j" означает, что вы должны выровнять текст по ширине - justify. 
+# И, наконец, строки вывода не должны заканчиваться пробелами.
+
+# Если вам нужно поместить суммарно непарное количество пробелов вокруг строки, 
+# чтобы отцентрировать ее, то поставьте парное количество пробелов вперед.
+
+# Правила выравнивания текста:
+
+#    Поскольку мы не всегда можем поставить одинаковое количество пробелов между словами в строке, 
+# поместите большие блоки пробелов сначала. Например: X---X---X--X--X--X, 
+# когда вам нужно поместить 12 пробелов в 5 местах: 3-3-2-2-2.
+#    Не выравнивайте последнюю строку текста.
+
+# Вам не нужно будет разбивать слово на две части, так как предоставленной ширины вполне достаточно.
+
+# Входные данные: Текст (строка - str), ширина (целое число - int) и стиль (строка - str).
+# Выходные данные: Отформатированный текст (строка - str).
+
+# Примеры:
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        38,
+        "l",
+    )
+    == "Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit. Iure\nharum suscipit aperiam aliquam ad,\nperferendis ex molestias reiciendis\naccusantium quos, tempore sunt quod\nveniam, eveniet et necessitatibus\nmollitia. Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        30,
+        "c",
+    )
+    == " Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit.\n Iure harum suscipit aperiam\n  aliquam ad, perferendis ex\n     molestias reiciendis\naccusantium quos, tempore sunt\n   quod veniam, eveniet et\n   necessitatibus mollitia.\n        Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        50,
+        "r",
+    )
+    == "           Lorem ipsum dolor sit amet, consectetur\n     adipisicing elit. Iure harum suscipit aperiam\n   aliquam ad, perferendis ex molestias reiciendis\n       accusantium quos, tempore sunt quod veniam,\n eveniet et necessitatibus mollitia. Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        45,
+        "j",
+    )
+    == "Lorem   ipsum  dolor  sit  amet,  consectetur\nadipisicing elit. Iure harum suscipit aperiam\naliquam    ad,   perferendis   ex   molestias\nreiciendis  accusantium  quos,  tempore  sunt\nquod   veniam,   eveniet   et  necessitatibus\nmollitia. Quasi, culpa."
+)
+
+# Предварительное условие:
+
+# -    all(len(word) <= width for word in text.split());
+# -    '\n' нету в тексте;
+# -    варианты стиля в ("l", "c", "r", "j");
+# -    0 < len(text) <= 1000.
 
 # ___________________________________________________________________________________
 # SOLUTION 6. <>
