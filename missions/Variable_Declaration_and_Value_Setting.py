7e4d2ae841f034abe21de5e25c88da697e2c8153
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-11-20 12:53:11 +0300

    Create Variable_Declaration_and_Value_Setting.py

---
+++ missions/Variable_Declaration_and_Value_Setting.py
@@ -0,0 +1,37 @@
+# Variable: Declaration and Value Setting >< 
+# The very beginning  ? 
+# Elementary  <>
+# 
+# ___________________________________________________________________________________
+#   Elementary
+# DE English FR PL UK ZH-HANS
+
+# 1. In this very first mission you just need to declare two variables a, b and set their values to integers 2, 5 respectively.
+# 2. When done, try to set values for variables in a single line (first variables gets first value etc.).
+# While Python remains a dynamically typed language, 
+# meaning you don't have to specify types for variables, incorporating type annotations, 
+# especially in larger projects or when working in a team, 
+# can significantly enhance code quality and maintainability. 
+# It's worth noting that type annotations are optional, 
+# and Python will not enforce strict typing; 
+# they serve primarily as a development aid and documentation tool.
+
+# 3. For integers use int type. Rewrite the first variant with type annotation. 
+
+# 1. В этой самой первой миссии вам просто нужно объявить две переменные 
+# a, b и установить их значения целыми числами 2, 5 соответственно.
+
+# 2. По завершении попробуйте задать значения переменных в одной строке (первые переменные получают первое значение и т. д.).
+
+# Хотя Python остается динамически типизированным языком, 
+# то есть вам не нужно указывать типы для переменных, 
+# включение аннотаций типов, особенно в крупных проектах или при работе в команде, 
+# может значительно повысить качество кода и удобство сопровождения. 
+# Стоит отметить, что аннотации типов не являются обязательными, 
+# и Python не обеспечивает строгую типизацию; 
+# они служат в первую очередь средством помощи в целях развития и документирования.
+
+# 3. Для целых чисел используйте тип int. Перепишите первый вариант с аннотацией типа.
+# ___________________________________________________________________________________
+# SOLUTION <>
+a, b = int(2), int(5)
