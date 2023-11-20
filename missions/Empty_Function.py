54c542e0f01832406dd4f8553c1b2811f1511534
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-11-20 13:06:53 +0300

    Create Empty_Function.py

---
+++ missions/Empty_Function.py
@@ -0,0 +1,44 @@
+# ___________________________________________________________________________________
+# Empty Function >< 
+# Empty function  ? 
+# Elementary <>
+# --
+# ___________________________________________________________________________________
+#
+# Elementary
+# DE English FR PL UK ZH-HANS
+
+# Keeping in memory your knowledge about variables and operations 
+# with them let's make a step aside and talk about functions. 
+# Function is a block of code, that may be used multiple times in different places. 
+# Usually function gets some input data (parameters), 
+# make some operations with them and returns some value outside.
+
+# 1. In this mission you need to define a function func, which takes no arguments.
+# 2. Your function is also not going to do anything! 
+# To keep a code (expression) working correctly without doing anything 
+# you need to use so called placeholders: keyword pass or ... operator.
+
+# 3. Note that even if a function returns nothing, 
+# it returns the empty value None anyway. 
+
+# Элементарный
+# DE Английский FR PL UK ZH-HANS
+
+# Сохранение в памяти ваших знаний о переменных и операциях
+# вместе с ними сделаем шаг в сторону и поговорим о функциях.
+# Функция — это блок кода, который можно использовать несколько раз в разных местах.
+# Обычно функция получает некоторые входные данные (параметры),
+# делаем с ними какие-то операции и возвращаем какое-то значение снаружи.
+
+# 1. В этой миссии вам нужно определить функцию func, которая не принимает аргументов.
+# 2. Ваша функция тоже ничего делать не собирается!
+# Чтобы код (выражение) работал правильно, ничего не делая
+# вам нужно использовать так называемые заполнители: ключевое слово pass или ... оператор.
+
+# 3. Обратите внимание: даже если функция ничего не возвращает,
+# он все равно возвращает пустое значение None.
+# ___________________________________________________________________________________
+# SOLUTION <>
+def func():
+    return None
