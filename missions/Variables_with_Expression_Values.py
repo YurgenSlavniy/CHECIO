b15ef315a18a919d93fc41be9f70cece3547f504
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-11-20 12:59:13 +0300

    Create Variables_with_Expression_Values.py

---
+++ missions/Variables_with_Expression_Values.py
@@ -0,0 +1,33 @@
+# ___________________________________________________________________________________
+# Variables with Expression Values >< 
+# Add and Multiply  ? 
+# Elementary <>
+# --
+# ___________________________________________________________________________________
+#  Elementary
+# DE English FR PL UK ZH-HANS
+
+# 1. Let's introduce two new variables named add, multi. 
+# Assign them values equal to the sum and product, 
+# respectively, of the previously declared variables a, b.
+# 2. Utilizing expressions enables you to easily modify the values of 
+# a, b later while ensuring accurate values for add, multi. 
+# Feel free to adjust a, b and then press Check Solution. 
+# If you express add, multi, you'll still successfully complete this task. 
+
+# 1. Давайте введем две новые переменные с именами add, multi.
+# Присвойте им значения, равные сумме и произведению,
+# соответственно ранее объявленных переменных a, b.
+# 2. Использование выражений позволяет легко изменять значения
+# a, b позже, гарантируя точные значения для add, multi.
+# Не стесняйтесь отрегулировать a, b, а затем нажмите «Проверить решение».
+# Если вы выразите add, multi, вы все равно успешно выполните эту задачу.
+# ___________________________________________________________________________________
+# SOLUTION <>
+# Taken from mission Variable: Declaration and Value Setting
+
+# write your code here
+a, b = int(2), int(5)
+# write your code here
+add = a + b
+multi = a * b
