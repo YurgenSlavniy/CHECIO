1fbfb3fbfb11dfb199cb039782a689bdca8f8a28
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-11-24 13:47:19 +0300

    Create Longest_Common_Prefix.py

---
+++ missions/Longest_Common_Prefix.py
@@ -0,0 +1,31 @@
+# ___________________________________________________________________________________
+# Longest Common Prefix >< 
+# What we have in common ? 
+# Undefined <>
+# list string --
+# ___________________________________________________________________________________
+#  Undefined
+# DE English FR PL UK ZH-HANS
+
+# This function should take an list of strings and 
+# determine the longest common prefix among all the strings. 
+# If there is no common prefix, it should return an empty string.
+
+# example
+# Input: List of strings (str).
+# Output: String (str).
+
+# Examples:
+assert longest_prefix(["flower", "flow", "flight"]) == "fl"
+assert longest_prefix(["dog", "racecar", "car"]) == ""
+assert longest_prefix(["apple", "application", "appetizer"]) == "app"
+assert longest_prefix(["a"]) == "a"
+
+# How it’s used:
+
+# -    in autocomplete systems to suggest the most likely completion;
+# -    in file systems to suggest directory names based on current input;
+# -   in DNA sequence alignment to identify common substrings.
+
+# ___________________________________________________________________________________
+# SOLUTION <>
