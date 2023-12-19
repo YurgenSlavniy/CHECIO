8a6a9faee8443f966e3807fd9a353a413d21baf8
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-12-19 11:28:32 +0300

    Update Longest_Common_Prefix.py

--- missions/Longest_Common_Prefix.py
+++ missions/Longest_Common_Prefix.py
@@ -29,3 +29,46 @@ assert longest_prefix(["a"]) == "a"
 
 # ___________________________________________________________________________________
 # SOLUTION <>
+
+def longest_prefix(arr: list[str]) -> str:
+    if len(arr[0]) == 0:
+        return ''
+    else: 
+  
+        temp_list = []
+        i = 0
+        len_list = []
+        for el in arr:
+            len_list.append(len(el))
+        min_len = min(len_list)
+        while i < min_len + 1:
+            temp = []
+            for el in arr:
+                temp.append(el[:i])
+            temp_list.append(temp)
+            i += 1
+    
+    temp_set = []
+    for el in temp_list:
+        if len(set(el)) == 1:
+            temp_set.append(el)
+
+    return temp_set[-1][0]
+  
+                
+
+print("Example:")
+print(longest_prefix(["flower", "flow", "flight"]))
+
+# These "asserts" are used for self-checking
+assert longest_prefix(["flower", "flow", "flight"]) == "fl"
+assert longest_prefix(["dog", "racecar", "car"]) == ""
+assert longest_prefix(["apple", "application", "appetizer"]) == "app"
+assert longest_prefix(["a"]) == "a"
+assert longest_prefix(["", "b"]) == ""
+assert longest_prefix(["same", "same", "same"]) == "same"
+assert longest_prefix(["mismatch", "mister", "miss"]) == "mis"
+assert longest_prefix(["alphabet", "alpha", "alphadog"]) == "alpha"
+assert longest_prefix(["book", "boot", "booster"]) == "boo"
+assert longest_prefix(["short", "shore", "shot"]) == "sho"
+
