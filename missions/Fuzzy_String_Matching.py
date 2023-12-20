82f37685bac69220e8aa7ac35f098a3855d54b46
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-12-20 10:24:32 +0300

    Create Fuzzy_String_Matching.py

---
+++ missions/Fuzzy_String_Matching.py
@@ -0,0 +1,44 @@
+# Fuzzy String Matching >< 
+# Fuzzily equal strings ? 
+# Undefined <>
+# string --
+# ___________________________________________________________________________________
+# Undefined
+# DE English FR PL UK ZH-HANS
+
+# Given two strings and a permissible number of character differences, 
+# determine if the strings can be considered approximately equal.
+
+# example
+# Input: Three arguments: two strings (str) and one integer (int).
+# Output: Logic value (bool).
+
+# Examples:
+assert fuzzy_string_match("apple", "appel", 2) == True
+assert fuzzy_string_match("apple", "bpple", 1) == True
+assert fuzzy_string_match("apple", "bpple", 0) == False
+assert fuzzy_string_match("apple", "apples", 1) == True
+
+# How it’s used:
+
+#    spell-checking algorithms where slight differences can be ignored;
+#    searching algorithms in databases to find entries that closely match the query;
+#    DNA sequence matching where certain differences might be allowed.
+
+# Precondition:
+
+#    0 <= threshold <= max(len(str1), len(str2)).
+# ___________________________________________________________________________________
+# SOLUTION  <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+
+# <><><><><> Best "Creative" Solution <><><><><>
+
+# <><><><><> Best "Speedy" Solution <><><><><>
+
+# <><><><><> Best "3rd party" Solution <><><><><>
+
+# <><><><><> Uncategorized <><><><><>
+
+# ___________________________________________________________________________________
