5a3ab7ec3015c5a7db34425a669f8728e169a0c3
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-12-26 16:11:58 +0300

    Create All_Permutations .py

---
+++ missions/All_Permutations .py
@@ -0,0 +1,64 @@
+# All Permutations>< 
+# Show me every permutation!  ? 
+# Undefined <>
+# math string  --
+# ___________________________________________________________________________________
+# Undefined
+# English
+
+# Given a string, return all possible permutations of its characters, sorted alphabetically.
+
+# example
+# Input: String (str).
+# Output: Iterable of strings (str).
+
+# Examples:
+assert list(string_permutations("ab")) == ["ab", "ba"]
+assert list(string_permutations("abc")) == ["abc", "acb", "bac", "bca", "cab", "cba"]
+assert list(string_permutations("a")) == ["a"]
+assert list(string_permutations("abcd")) == [
+    "abcd",
+    "abdc",
+    "acbd",
+    "acdb",
+    "adbc",
+    "adcb",
+    "bacd",
+    "badc",
+    "bcad",
+    "bcda",
+    "bdac",
+    "bdca",
+    "cabd",
+    "cadb",
+    "cbad",
+    "cbda",
+    "cdab",
+    "cdba",
+    "dabc",
+    "dacb",
+    "dbac",
+    "dbca",
+    "dcab",
+    "dcba",
+]
+
+# How it’s used:
+# - puzzles and games like Scrabble where word combinations matter;
+# - cryptography to test possible keys given a set of characters;
+# - data analysis in genetics for possible gene combinations.
+
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+
+# <><><><><> Best "Creative" Solution <><><><><>
+
+# <><><><><> Best "Speedy" Solution <><><><><>
+
+# <><><><><> Best "3rd party" Solution <><><><><>
+
+# <><><><><> Uncategorized <><><><><>
+
+# ___________________________________________________________________________________
