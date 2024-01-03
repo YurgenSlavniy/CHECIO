3a84404b3f071c8de8c6f1cfd1282b3d367b7a6a
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-03 11:28:39 +0300

    Update Words_Order.py

--- missions/Words_Order.py
+++ missions/Words_Order.py
@@ -76,11 +76,65 @@ assert (
 # то мы возвращаем True.
 
 # <><><><><> Best "Clear" Solution <><><><><>
+def words_order(text, words):
+    text_words = {w for w in text.split() if w in words}
+    return list(sorted(text_words, key=text.index)) == words
 
 # <><><><><> Best "Creative" Solution <><><><><>
+import re
+
+def words_order(text, words):
+    return len(set(words)) == len(words) and \
+        not not re.search(r'\b' + r'\b.*\b'.join(words) + r'\b', text)
 
 # <><><><><> Best "Speedy" Solution <><><><><>
+def words_order(text: str, words: list) -> bool:
+    # A word that appears twice make this simple.
+    if len(set(words)) != len(words):
+        return False
+    # Look for words indexes with a simple iteration on text words.
+    words = {word: -1 for word in words}  # A dict remembers insertion order.
+    for n, text_word in enumerate(text.split()):
+        if text_word in words and words[text_word] == -1:
+            words[text_word] = n
+    # Make sure all words are in the text and indexes are increasing.
+    last = -1
+    for index in words.values():
+        if index <= last:
+            return False
+        last = index
+    return True
 
 # <><><><><> Best "3rd party" Solution <><><><><>
+import numpy as np
+
+def words_order(text: str, words: list) -> bool:
+
+    text_split = text.split(" ")
+    lg = len(words)
+    
+    count_list = np.array([text_split.count(x) for x in words])
+    # print(count_list)
+    if np.sum(count_list != 0) == lg:
+        index_list = np.array([text_split.index(x) for x in words])
+        # print(index_list)
+        return np.sum(index_list == np.sort(index_list)) == lg\
+            and len(np.unique(index_list)) == lg            
+    else:
+        return False
 
 # <><><><><> Uncategorized <><><><><>
+def words_order(text: str, words: list) -> bool:
+    without_duplicate = list(set(words))
+    if len(without_duplicate) != len(words):
+        return False
+
+    list_text = text.split(" ")
+
+    unique_list = []
+
+    for item in list_text:
+        if item not in unique_list and item in words:
+            unique_list.append(item)
+
+    return unique_list == words
