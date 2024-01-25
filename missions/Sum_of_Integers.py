050930e19a76e0252f40937bf4f4b51fa8403aa4
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-25 21:00:46 +0300

    Create  Sum_of_Integers.py

---
+++ missions/Sum_of_Integers.py
@@ -0,0 +1,35 @@
+# Sum of Integers >< 
+# From 1 to N...go!  ? 
+# Undefined <>
+# math numbers --
+# ___________________________________________________________________________________
+# Undefined
+# DE English FR PL UK ZH-HANS
+
+# Calculate sum of integers from 1 to given N (including).
+
+# Input: Integer (int).
+# Output: Integer (int).
+
+# Examples:
+assert sum_upto_n(1) == 1
+assert sum_upto_n(2) == 3
+assert sum_upto_n(3) == 6
+assert sum_upto_n(4) == 10
+
+# How it’s used: this function can be used in a variety 
+# of mathematical and algorithmic contexts, such as in calculating 
+# the nth triangular number, determining the cost for certain operations in algorithms, etc.
+
+# Precondition:
+# -    1 ≤ N ≤ 900.
+
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
