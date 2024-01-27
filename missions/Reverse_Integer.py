ac46092df88936f0d6488fe924009529911c6c3f
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-27 21:47:45 +0300

    Create Reverse_Integer.py

---
+++ missions/Reverse_Integer.py
@@ -0,0 +1,38 @@
+# Reverse Integer >< 
+# Reflect a number  ? 
+# Undefined <>
+# numbers --
+# ___________________________________________________________________________________
+#  Undefined
+# DE English FR PL UK ZH-HANS
+
+# Reverse the digits of a given integer. 
+# For instance, 1234 should become 4321.
+# For negative integers, the sign should remain in the front; e.g., -123 becomes -321.
+
+# Input: Integer (int).
+# Output: Integer (int).
+
+# Examples:
+assert reverse_digits(1234) == 4321
+assert reverse_digits(0) == 0
+assert reverse_digits(-123) == -321
+assert reverse_digits(5) == 5
+
+# How it’s used: this function can be utilized in 
+# various algorithmic challenges, 
+# number-based puzzles, and certain numerical calculations.
+
+# Precondition:
+# -  −109 ≤ num ≤ 109.
+ 
+
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
