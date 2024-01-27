e51e0bd50005302a08634969b6c5feb2a0cf79ea
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-27 21:48:45 +0300

    Update Reverse_Integer.py

--- missions/Reverse_Integer.py
+++ missions/Reverse_Integer.py
@@ -29,6 +29,29 @@ assert reverse_digits(5) == 5
 
 # ___________________________________________________________________________________
 # SOLUTION <>
+def reverse_digits(num: int) -> int:
+    if num >= 0:    
+        string = str(num)
+        return int(string[::-1])
+    else:
+        string = str(num)[1:]
+        return 0 - int(string[::-1])
+
+print("Example:")
+print(reverse_digits(32))
+
+# These "asserts" are used for self-checking
+assert reverse_digits(1234) == 4321
+assert reverse_digits(0) == 0
+assert reverse_digits(-123) == -321
+assert reverse_digits(5) == 5
+assert reverse_digits(-9) == -9
+assert reverse_digits(100) == 1
+assert reverse_digits(-100) == -1
+assert reverse_digits(54321) == 12345
+assert reverse_digits(1111) == 1111
+assert reverse_digits(987654321) == 123456789
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
