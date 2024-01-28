5694762532b49eafda32663c55420bdebfa22161
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-28 12:44:02 +0300

    Create Maximum_Among_Three.py

---
+++ missions/Maximum_Among_Three.py
@@ -0,0 +1,35 @@
+# Maximum Among Three >< 
+# There are three of us...one must remains. ? 
+#  <>
+# numbers--
+# ___________________________________________________________________________________
+# Undefined
+# DE English FR PL UK ZH-HANS
+
+# Given three integers, determine which one is the largest.
+
+# Input: Three integers (int).
+# Output: Integer (int).
+
+# Examples:
+assert max_of_three(1, 2, 3) == 3
+assert max_of_three(3, 2, 1) == 3
+assert max_of_three(1, 3, 2) == 3
+assert max_of_three(0, 0, 0) == 0
+
+# How it’s used: 
+# - this function can be used in various algorithms 
+# and computations where the highest value among multiple options needs to be selected.
+
+# Precondition:
+
+# -    -109 <= a, b, c <= 109.
+ 
+ __________________________________________________________________________________# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
