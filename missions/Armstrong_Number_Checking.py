949544c7819f9cb6e898008af5814c487886a1de
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-28 12:36:09 +0300

    Create Armstrong_Number_Checking.py

---
+++ missions/Armstrong_Number_Checking.py
@@ -0,0 +1,36 @@
+# Armstrong Number Checking >< 
+# Narcissistic number ? 
+# Undefined <>
+# bool math numbers --
+# ___________________________________________________________________________________
+# Undefined
+# DE English FR PL UK ZH-HANS
+
+# In number theory, an Armstrong number (after Michael F. Armstrong) 
+# or narcissistic number in a given number base 
+# (10 for this mission) is a number that is the sum of 
+# its own digits each raised to the power of the number of digits.
+# For example, 153 is an Armstrong number because 13 + 53 + 33 == 153.
+
+# Input: Integer (int).
+# Output: Logic value (bool).
+
+# Examples:
+assert is_armstrong(153) == True
+assert is_armstrong(370) == True
+assert is_armstrong(947) == False
+assert is_armstrong(371) == True
+
+# How it’s used: 
+# - this function can be employed in mathematical puzzles, 
+# encryption algorithms, and educational tools.  
+
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
