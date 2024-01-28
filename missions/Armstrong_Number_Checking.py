4f06296766323263d0993759b5d16c77511ae976
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-28 12:36:53 +0300

    Update Armstrong_Number_Checking.py

--- missions/Armstrong_Number_Checking.py
+++ missions/Armstrong_Number_Checking.py
@@ -27,7 +27,26 @@ assert is_armstrong(371) == True
 
 # ___________________________________________________________________________________
 # SOLUTION <>
+def is_armstrong(num: int) -> bool:
+    len_num = len(str(num)) 
+    armstrong_sum = sum(int(digit)**len_num for digit in str(num))
+    return armstrong_sum == num
 
+
+print("Example:")
+print(is_armstrong(11))
+
+# These "asserts" are used for self-checking
+assert is_armstrong(153) == True
+assert is_armstrong(370) == True
+assert is_armstrong(947) == False
+assert is_armstrong(371) == True
+assert is_armstrong(407) == True
+assert is_armstrong(163) == False
+assert is_armstrong(100) == False
+assert is_armstrong(8208) == True
+assert is_armstrong(930) == False
+assert is_armstrong(4) == True
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
 # <><><><><> Best "Speedy" Solution <><><><><>
