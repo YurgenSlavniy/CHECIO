2b174af2aa9543346b95854c55fef97186d4d325
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-29 08:29:42 +0300

    Create Leap_Year_Checking.py

---
+++ missions/Leap_Year_Checking.py
@@ -0,0 +1,36 @@
+# Leap Year Checking >< 
+# Not every year is like this one ? 
+# Undefined <>
+# bool numbers --
+# ___________________________________________________________________________________
+# Undefined
+# DE English FR PL UK ZH-HANS
+
+# Check if the given year is leap year.
+# A year is a leap year if it is divisible by 4, 
+# except for end-of-century years which must be divisible by 400. 
+# This means that the year 2000 was a leap year, although 1900 was not.
+
+# Input: Integer (int).
+# Output: Logic value (bool).
+
+# Examples:
+assert is_leap_year(2000) == True
+assert is_leap_year(1900) == False
+assert is_leap_year(2004) == True
+assert is_leap_year(2100) == False
+
+# How it’s used: 
+# - this function can be used in calendars, scheduling applications, or any system which deals with yearly data.
+
+# Precondition:
+# - 1 <= year <= 105
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
