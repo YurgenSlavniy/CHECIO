d9561f7c7cb2aaf4d1f652fdab78ff38423cc265
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-01-29 08:20:07 +0300

    Update Maximum_Among_Three.py

--- missions/Maximum_Among_Three.py
+++ missions/Maximum_Among_Three.py
@@ -25,7 +25,25 @@ assert max_of_three(0, 0, 0) == 0
 
 # -    -109 <= a, b, c <= 109.
  
- __________________________________________________________________________________# SOLUTION <>
+ __________________________________________________________________________________
+# SOLUTION <>
+def max_of_three(a: int, b: int, c: int) -> int:
+    return max([a,b,c])
+
+print("Example:")
+print(max_of_three(1, 2, 3))
+
+# These "asserts" are used for self-checking
+assert max_of_three(1, 2, 3) == 3
+assert max_of_three(3, 2, 1) == 3
+assert max_of_three(1, 3, 2) == 3
+assert max_of_three(0, 0, 0) == 0
+assert max_of_three(-1, -2, -3) == -1
+assert max_of_three(5, 5, 4) == 5
+assert max_of_three(-5, -5, -6) == -5
+assert max_of_three(10, 9, 10) == 10
+assert max_of_three(123, 456, 789) == 789
+assert max_of_three(789, 123, 456) == 789
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
