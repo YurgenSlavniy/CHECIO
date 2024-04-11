a71dd8019afe51567cdb024e7ab72ed739b181be
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-11 10:41:44 +0300

    Update All_Upper_I.py

--- missions/All_Upper_I.py
+++ missions/All_Upper_I.py
@@ -42,9 +42,51 @@ assert is_all_upper("") == True
 assert is_all_upper("444") == True
 assert is_all_upper("55 55 5 ") == True
 
+
 # <><><><><> Best "Clear" Solution <><><><><>
+def is_all_upper(text: str) -> bool:
+    return text.upper() == text
+
+
 # <><><><><> Best "Creative" Solution <><><><><>
+def is_all_upper(text: str) -> bool:
+    text = text + "A"
+    return text.isupper()
+
+
 # <><><><><> Best "Speedy" Solution <><><><><>
+def is_all_upper(text: str) -> bool:
+    return not any(ch.islower() for ch in text)
+
+
 # <><><><><> Best "3rd party" Solution <><><><><>
+from contextlib import suppress
+from functools import reduce
+from sympy import prime
+from math import gcd
+
+LOWER = 1384096573029127477274111072084485853924710240181310093982703945448241409
+
+'''
+LOWER = prime(ord('a')) * prime(ord('b')) * ... * prime(ord('z'))
+therefore if non empty string doesn't contain any lower case then
+gcd(ord(t[0])) * prime(ord(t[1])) * ... * prime(ord(t[-1]), LOWER) == 1 
+'''
+
+def is_all_upper(text):
+    with suppress(TypeError):
+        product = reduce(int.__mul__, map(prime, map(ord, text+'A')))
+        return gcd(product, LOWER) == 1
+    return False
+
+
 # <><><><><> Uncategorized <><><><><>
+
+def is_all_upper(text: str) -> bool:
+    # ascii code Z => 90
+    char_list = list(text)
+    # all(): return true if all item is true/iterable
+    return all(ord(x) <= 90 for x in char_list)
+
+
 # ___________________________________________________________________________________
