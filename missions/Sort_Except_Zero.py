0826c11879b9be03d89d592d1390e3dc166e6b6a
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-12 12:03:54 +0300

    Create  Sort_Except_Zero.py

---
+++ missions/Sort_Except_Zero.py
@@ -0,0 +1,30 @@
+# Sort Except Zero >< 
+# Zeros should not be changed ? 
+# Simple <>
+# list math --
+# ___________________________________________________________________________________
+# Simple
+# DE English FR PL UK ZH-HANS
+
+# Sort the integers in sequence in sequence. But the position of zeros should not be changed.
+
+# Input: List of integers (int).
+# Output: List or another Iterable (tuple, generator, iterator) of integers (int).
+
+# Examples:
+assert list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])) == [1, 3, 0, 0, 4, 4, 5, 0, 7]
+assert list(except_zero([0, 2, 3, 1, 0, 4, 5])) == [0, 1, 2, 3, 0, 4, 5]
+assert list(except_zero([0, 0, 0, 1, 0])) == [0, 0, 0, 1, 0]
+assert list(except_zero([4, 5, 3, 1, 1])) == [1, 1, 3, 4, 5]
+
+# Precondition:
+# All numbers are non-negative.
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
