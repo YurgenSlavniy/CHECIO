d838c7e2638758016de87e173bf01f3b3534bd9f
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-12 16:12:19 +0300

    Update Zigzag_Array.py

--- missions/Zigzag_Array.py
+++ missions/Zigzag_Array.py
@@ -25,6 +25,28 @@ assert create_zigzag(3, 3, 5) == [[5, 6, 7], [10, 9, 8], [11, 12, 13]]
 # It is taught for Ryerson Chang School of Continuing Education by Ilkka Kokkarinen
 # ___________________________________________________________________________________
 # SOLUTION <>
+import numpy as np
+
+def create_zigzag(rows: int, cols: int, start: int = 1) -> list[list[int]]:
+    matrix = np.arange(start, start + rows*cols).reshape(rows,cols)
+    result_matrix = []
+    for el in range(0, len(matrix)):
+        if el%2 != 0:
+            result_matrix.append(list(matrix[el][::-1]))
+        else:
+            result_matrix.append(list(matrix[el]))
+            
+    return result_matrix
+
+
+print("Example:")
+print(create_zigzag(3, 5))
+
+# These "asserts" are used for self-checking
+assert create_zigzag(3, 5) == [[1, 2, 3, 4, 5], [10, 9, 8, 7, 6], [11, 12, 13, 14, 15]]
+assert create_zigzag(5, 1) == [[1], [2], [3], [4], [5]]
+assert create_zigzag(3, 3, 5) == [[5, 6, 7], [10, 9, 8], [11, 12, 13]]
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
