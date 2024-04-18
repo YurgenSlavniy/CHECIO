19eff52b6166284bb6f696840dff10cb57faf2e7
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-18 16:43:46 +0300

    Update Missing_Number.py

--- missions/Missing_Number.py
+++ missions/Missing_Number.py
@@ -22,6 +22,24 @@ assert missing_number([2, 6, 8]) == 4
 # - отсутствующий элемент всегда находится между двумя элементами в последовательности. 
 # ___________________________________________________________________________________
 # SOLUTION <>
+def missing_number(items: list[int]) -> int:
+    idxs = range(0, len(items) - 1)
+    sort_items = sorted(items)
+    steps = []
+    for el in idxs:
+        steps.append(sort_items[el+1] - sort_items[el])
+    
+    step = min(set(steps))
+    start_el = sort_items[0]
+    finish_el = sort_items[-1]
+    
+    itgrs = list(range(start_el, finish_el+1, step))
+    result_digit = set(itgrs) - set(sort_items)
+    for el in result_digit:
+        result = int(el)
+    
+    return result
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
