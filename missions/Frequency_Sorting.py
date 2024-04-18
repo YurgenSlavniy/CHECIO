770cdc4fa5c4c1ab892ac425a5436cf4d6567328
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-18 13:35:15 +0300

    Update Frequency_Sorting.py

--- missions/Frequency_Sorting.py
+++ missions/Frequency_Sorting.py
@@ -38,6 +38,43 @@ assert list(frequency_sorting([3, 4, 11, 13, 11, 4, 4, 7, 3])) == [
 # - max number <= 100
 # ___________________________________________________________________________________
 # SOLUTION <>
+from collections.abc import Iterable
+
+
+def frequency_sorting(numbers: list[int]) -> Iterable[int]:
+    dig_count = {}
+    for el in sorted(set(numbers)):
+        dig_count[el] = numbers.count(el)
+    
+    sorted_dig_dict = sorted(dig_count.items(), key=lambda item: item[1], reverse=True)
+    result = []
+    for el in sorted_dig_dict:
+        i = el[1]
+        while i > 0:
+            result.append(el[0]) 
+            i-=1
+    
+    return result
+
+
+print("Example:")
+# print(list(frequency_sorting([1, 2, 3, 4, 5])))
+print(frequency_sorting([3, 4, 11, 13, 11, 4, 4, 7, 3]))
+
+# These "asserts" are used for self-checking
+assert list(frequency_sorting([1, 2, 3, 4, 5])) == [1, 2, 3, 4, 5]
+assert list(frequency_sorting([3, 4, 11, 13, 11, 4, 4, 7, 3])) == [
+    4,
+    4,
+    4,
+    3,
+    3,
+    11,
+    11,
+    7,
+    13,
+]
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
