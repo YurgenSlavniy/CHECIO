7fced34b1c6549e5fa7a1f607ea533a6caba698f
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-23 10:49:18 +0300

    Update Text_Formatting.py

--- missions/Text_Formatting.py
+++ missions/Text_Formatting.py
@@ -70,6 +70,85 @@ assert (
 # - 0 < len(text) <= 1000.
 # ___________________________________________________________________________________
 # SOLUTION <>
+from textwrap import wrap
+
+
+def rjust(line, width):
+    return line.rjust(width)
+
+
+def ljust(line, width):
+    return line
+
+
+def cjust(line, width):
+    return line.center(width).rstrip()
+
+
+def jjust(line, width):
+    words = line.split()
+    space_to_fill = width - sum(len(word) for word in words)
+    spaces = len(words)-1
+    if not spaces:
+        return line
+    sep_width, wide_sep_count = divmod(space_to_fill, spaces)
+    sep = ' '*sep_width
+    return sep.join(words).replace(sep, sep+' ', wide_sep_count)
+
+
+def text_formatting(text, width, style):
+    d = dict(zip('rlcj', (rjust, ljust, cjust, jjust)))
+    justify = d[style]
+    lines = [justify(line, width) for line in wrap(text, width)]
+    if style == 'j':  # De-justify last line
+        last_line = lines.pop()
+        lines.append(' '.join(last_line.split()))
+    return '\n'.join(lines)
+
+
+LINE = (
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure "
+    "harum suscipit aperiam aliquam ad, perferendis ex molestias "
+    "reiciendis accusantium quos, tempore sunt quod veniam, eveniet "
+    "et necessitatibus mollitia. Quasi, culpa."
+)
+print("Example:")
+print(text_formatting(LINE, 38, "l"))
+
+# These "asserts" are used for self-checking
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        38,
+        "l",
+    )
+    == "Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit. Iure\nharum suscipit aperiam aliquam ad,\nperferendis ex molestias reiciendis\naccusantium quos, tempore sunt quod\nveniam, eveniet et necessitatibus\nmollitia. Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        30,
+        "c",
+    )
+    == " Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit.\n Iure harum suscipit aperiam\n  aliquam ad, perferendis ex\n     molestias reiciendis\naccusantium quos, tempore sunt\n   quod veniam, eveniet et\n   necessitatibus mollitia.\n        Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        50,
+        "r",
+    )
+    == "           Lorem ipsum dolor sit amet, consectetur\n     adipisicing elit. Iure harum suscipit aperiam\n   aliquam ad, perferendis ex molestias reiciendis\n       accusantium quos, tempore sunt quod veniam,\n eveniet et necessitatibus mollitia. Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        45,
+        "j",
+    )
+    == "Lorem   ipsum  dolor  sit  amet,  consectetur\nadipisicing elit. Iure harum suscipit aperiam\naliquam    ad,   perferendis   ex   molestias\nreiciendis  accusantium  quos,  tempore  sunt\nquod   veniam,   eveniet   et  necessitatibus\nmollitia. Quasi, culpa."
+)
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
