ce063cbd8225a179cc87b9a102224a56fdc6a3ef
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-04-23 11:02:43 +0300

    Update Xs_and_Os_Referee.py

--- missions/Xs_and_Os_Referee.py
+++ missions/Xs_and_Os_Referee.py
@@ -69,8 +69,72 @@ assert checkio(["O.X", "XX.", "XOO"]) == "X"
 
 
 # <><><><><> Best "Clear" Solution <><><><><>
+def checkio(result):
+    rows = result
+    cols = map(''.join, zip(*rows))
+    diags = map(''.join, zip(*[(r[i], r[2 - i]) for i, r in enumerate(rows)]))
+    lines = rows + list(cols) + list(diags)
+
+    return 'X' if ('XXX' in lines) else 'O' if ('OOO' in lines) else 'D'
+
+
 # <><><><><> Best "Creative" Solution <><><><><>
+checkio=lambda r,j="".join:"DOX"[sum(m*3in[j(r[::e])[::4]for e
+in(1,-1)]+r+list(map(j,zip(*r)))for m in"XOX")]
+
+
 # <><><><><> Best "Speedy" Solution <><><><><>
+def checkio(game_result):
+    for i in range(3):
+        if (game_result[i][0] in ['O', 'X']) and (game_result[i][0] == game_result[i][1] == game_result[i][2]):
+            return game_result[i][0]
+        if (game_result[0][i] in ['O', 'X']) and (game_result[0][i] == game_result[1][i] == game_result[2][i]):
+            return game_result[0][i]
+    if (game_result[1][1] in ['O', 'X']) and ((game_result[0][0] == game_result[1][1] == game_result[2][2]) or (game_result[0][2] == game_result[1][1] == game_result[2][0])):
+        return game_result[1][1]
+    return "D"
+    
+
 # <><><><><> Best "3rd party" Solution <><><><><>
+import numpy as np
+
+def checkio(game_result):
+    data = np.array([d for item in game_result for d in item]).reshape(3, 3)
+    all_same_mark = lambda arr: np.where(arr)[0].size > 0
+    for mark in ('X', 'O'):
+        if any(all_same_mark(to_check) for to_check in
+               [(data == mark).all(axis=0), \
+               (data == mark).all(axis=1), \
+               (data.diagonal() == mark).all(axis=0), \
+               (np.fliplr(data).diagonal() == mark).all(axis=0)]):
+            return mark
+
+    return 'D'
+    
+
 # <><><><><> Uncategorized <><><><><>
+def checkio(game_result: list[str]) -> str:
+    for i in range(len(game_result)):
+        if game_result[i][0] == game_result[i][1] and game_result[i][0] == game_result[i][2]:
+            if game_result[i][0].isalpha():
+                return game_result[i][0]
+
+    for i in range(len(game_result)):
+        if game_result[0][i] == game_result[1][i] and game_result[0][i] == game_result[2][i]:
+            if game_result[0][i].isalpha():
+                return game_result[0][i]
+
+    if game_result[0][0] == game_result[1][1] and game_result[0][0] == game_result[2][2]:
+        if game_result[0][0].isalpha():
+            return game_result[0][0]
+        else: 
+            return 'D'
+
+    elif game_result[0][2] == game_result[1][1] and game_result[0][2] == game_result[2][0]:
+        if game_result[2][0].isalpha():
+            return game_result[2][0]
+        else:
+            return 'D'
+
+
 # ___________________________________________________________________________________
