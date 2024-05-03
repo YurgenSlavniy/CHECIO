415cacac018e7e9ba9eb4738d00d1c0be55ffed5
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-05-03 11:13:28 +0300

    Create Is_String_a_Number_(Part_II).py

---
+++ missions/Is_String_a_Number_(Part_II).py
@@ -0,0 +1,34 @@
+# Is String a Number? (Part II) >< 
+# Checks if the string is a valid number ? 
+# Elementary+ <>
+# numbers string --
+# ___________________________________________________________________________________
+# Elementary+
+# DE English FR PL UK ZH-HANS
+
+# You are given a string. 
+# Your function should return True if the string is a valid number
+# (contains only digits and "+-." at proper places), otherwise - False. Look at the mask:
+
+# [+- ][zero or more digits][.][zero or more digits]
+
+# Of course, not all parts are necessary (but at least one digit part is!). For example, 
+# "+10." or "-.55" are valid numbers, where part equal 0 is omitted.
+
+# Input: A string (str).
+# Output: Logic value (bool).
+
+# Examples:
+assert is_number("34") == True
+assert is_number("df") == False
+assert is_number("") == False
+assert is_number("+10.0") == True
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
