3d33f6c6d072ba67886edba7f7111007420482d0
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-05-04 21:51:13 +0300

    Update Sun_Angle.py

--- missions/Sun_Angle.py
+++ missions/Sun_Angle.py
@@ -62,8 +62,57 @@ assert sun_angle("12:15") == 93.75
 
 
 # <><><><><> Best "Clear" Solution <><><><><>
+def sun_angle(time):
+    h, m = list(map(int, time.split(':')))
+    angle = 15 * h + m / 4 - 90
+    return angle if 0 <= angle <= 180 else "I don't see the sun!"
+
+
 # <><><><><> Best "Creative" Solution <><><><><>
+sun_angle=lambda t:["I don't see the sun!",int(t[:2])*15+int(t[~1:])/4-90][6<=int(t[:2])+int(t[~1:])/60<=18]
+
+
 # <><><><><> Best "Speedy" Solution <><><><><>
+def sun_angle(time):
+    hh, mm = map(int, time.split(':'))
+    tt = hh*60+mm
+    
+    return "I don't see the sun!" if (tt < 360 or 1080 < tt) else (tt-360)*0.25
+
+
 # <><><><><> Best "3rd party" Solution <><><><><>
+from typing import Union
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+def sun_angle(time: str) -> Union[int, str]:
+    minutes = list(pd.date_range("06:00", "18:00", freq="1min").strftime('%H:%M'))
+    if time not in minutes:
+        return "I don't see the sun!"
+
+    degrees = np.linspace(0,180,len(minutes))
+    
+    return list(filter(lambda t: t[0] == time, zip(minutes,degrees)))[0][1]
+
+
 # <><><><><> Uncategorized <><><><><>
+from typing import Union
+from datetime import datetime as dt
+
+
+def sun_angle(time: str) -> Union[float, str]:
+    dawn = 0
+    dusk = 180
+    hour_degree = 15
+    min_degree = .25
+    hour, minute = dt.strptime(time, "%H:%M").hour, dt.strptime(time, "%H:%M").minute
+    if hour < 6:
+        return "I don't see the sun!"
+    
+    degrees = (hour - 6) * hour_degree + (minute * min_degree)
+
+    return degrees if dawn <= degrees <= dusk else "I don't see the sun!"
+    
+
 # ___________________________________________________________________________________
