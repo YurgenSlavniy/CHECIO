3bcf73e0a2b2c12940cc25f62e0034c787f0cad0
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-05-04 21:42:30 +0300

    Update Is_String_a_Number_(Part_II).py

--- missions/Is_String_a_Number_(Part_II).py
+++ missions/Is_String_a_Number_(Part_II).py
@@ -25,6 +25,41 @@ assert is_number("") == False
 assert is_number("+10.0") == True
 # ___________________________________________________________________________________
 # SOLUTION <>
+def is_number(val: str) -> bool:
+    if val == '' or val == '.':
+        return False
+    
+    if val[0] == '+' or val[0] == '-':
+        val = val[1:]
+        
+    if val[-1] == '.':
+        val =val[:-1]
+        
+    if val[0] == '.':
+        val = val[1:]    
+    
+    splt_val = val.split('.')
+    if len(splt_val) == 2 and splt_val[0].isdigit() and splt_val[1].isdigit():
+        return True
+        
+    return val.isdigit()
+
+
+print("Example:")
+print(is_number("10"))
+
+
+# These "asserts" are used for self-checking
+assert is_number("34") == True
+assert is_number("df") == False
+assert is_number("") == False
+assert is_number("+10.0") == True
+assert is_number("1OOO") == False
+assert is_number("1.") == True
+assert is_number("+.l") ==  False
+assert is_number("++1+.2-") == False
+assert is_number("3e4") == False
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
