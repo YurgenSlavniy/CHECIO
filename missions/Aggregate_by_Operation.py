705a2818b7b3dcd98e599c80b2154b998b1ea941
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-06-19 14:04:26 +0300

    Update Aggregate_by_Operation.py

--- missions/Aggregate_by_Operation.py
+++ missions/Aggregate_by_Operation.py
@@ -26,6 +26,54 @@ assert aggr_operation([("*a", 0), ("=a", 0), ("/a", 0), ("-a", -5)]) == {"a": 5}
 
 # ___________________________________________________________________________________
 # SOLUTION <>
+def aggr_operation(data: list[tuple[str, int]]) -> dict[str, int]:
+    
+    result = {}
+    for el in data:
+        if el[0][1:] not in result:
+            result[el[0][1:]] = el[-1]
+            if el[0][0] == "+":
+                result[el[0][1:]] = 0 + el[-1]
+            elif el[0][0] == "-":
+                result[el[0][1:]] = 0 - el[-1]
+            elif el[0][0] == "*":
+                result[el[0][1:]] = 1 * el[-1] 
+            elif el[0][0] == "/":
+                result[el[0][1:]] = el[-1] / 1
+            elif el[0][0] == "=":
+                result[el[0][1:]] = el[-1]
+     
+        else:
+            if el[0][0] == "+":
+                result[el[0][1:]] = result[el[0][1:]] + el[-1]
+            elif el[0][0] == "-":
+                result[el[0][1:]] = result[el[0][1:]] - el[-1]
+            elif el[0][0] == "*":
+                result[el[0][1:]] = result[el[0][1:]] * el[-1] 
+            elif el[0][0] == "/":
+                if el[-1] == 0:
+                    result[el[0][1:]] = result[el[0][1:]] / 1
+                else:
+                    result[el[0][1:]] = result[el[0][1:]] / el[-1]
+            elif el[0][0] == "=":
+                result[el[0][1:]] = el[-1]
+           
+    for el in result.items():
+        if el[1] == 0:
+            return {}
+                                            
+    return result    
+
+
+print("Example:")
+print(aggr_operation([("+a", 7), ("-b", 8), ("*a", 10)]))
+
+# These "asserts" are used for self-checking
+assert aggr_operation([("+a", 7), ("-b", 8), ("*a", 10)]) == {"a": 70, "b": -8}
+assert aggr_operation([]) == {}
+assert aggr_operation([("+a", 5), ("+a", -5), ("-a", 5), ("-a", -5)]) == {}
+assert aggr_operation([("*a", 0), ("=a", 0), ("/a", 0), ("-a", -5)]) == {"a": 5}
+
 
 # <><><><><> Best "Clear" Solution <><><><><>
 # <><><><><> Best "Creative" Solution <><><><><>
