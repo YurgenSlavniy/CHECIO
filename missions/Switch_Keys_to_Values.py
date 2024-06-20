e49ec45b3a94f0aa12dd84b7d20880f46057a58d
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-06-20 10:45:07 +0300

    Create Switch_Keys_to_Values.py

---
+++ missions/Switch_Keys_to_Values.py
@@ -0,0 +1,43 @@
+# Switch Keys to Values >< 
+# Switch keys and values in a dictionary ? 
+# Elementary+ <>
+# dict string --
+# ___________________________________________________________________________________
+#  Elementary+
+
+# You are given a dictionary, where keys and values are strings. 
+# Your function should return a dictionary as well, where keys and values 
+# from input dictionary are switched: 
+# input keys become output values and vice versa. Looks easy?
+# It is so! The only thing left to mention: 
+# the values in the result dictionary should be sets (so the input key(s) - the element(s) of the set).
+# Good luck!
+
+# Input: A dictionary.
+# Output: A dictionary.
+
+# Examples:
+assert switch_dict({"rouses": "red", "car": "red", "sky": "blue"}) == {
+    "red": {"car", "rouses"},
+    "blue": {"sky"},
+}
+assert switch_dict({"1": "one", "2": "two", "3": "one", "4": "two"}) == {
+    "one": {"1", "3"},
+    "two": {"2", "4"},
+}
+assert switch_dict({"a": "b", "b": "c", "c": "a"}) == {
+    "b": {"a"},
+    "c": {"b"},
+    "a": {"c"},
+}
+ 
+
+# ___________________________________________________________________________________
+# SOLUTION <>
+
+# <><><><><> Best "Clear" Solution <><><><><>
+# <><><><><> Best "Creative" Solution <><><><><>
+# <><><><><> Best "Speedy" Solution <><><><><>
+# <><><><><> Best "3rd party" Solution <><><><><>
+# <><><><><> Uncategorized <><><><><>
+# ___________________________________________________________________________________
