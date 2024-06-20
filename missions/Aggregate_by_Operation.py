9dfb2ea5bcc076df8f6f4806b8871d37896550cc
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2024-06-20 10:52:30 +0300

    Update Aggregate_by_Operation.py

--- missions/Aggregate_by_Operation.py
+++ missions/Aggregate_by_Operation.py
@@ -76,8 +76,71 @@ assert aggr_operation([("*a", 0), ("=a", 0), ("/a", 0), ("-a", -5)]) == {"a": 5}
 
 
 # <><><><><> Best "Clear" Solution <><><><><>
+import collections
+
+def aggr_operation(data: list[tuple[str, int]]) -> dict[str, int]:
+    C = collections.Counter()
+    for (op, *key), value in data:
+        if key := ''.join(key):
+            match op:
+                case '=':     C[key]  = value
+                case '+':     C[key] += value
+                case '-':     C[key] -= value
+                case '*':     C[key] *= value
+                case '/':
+                    if value: C[key] /= value
+        if not C[key]:    del C[key]
+    return C
+  
+
 # <><><><><> Best "Creative" Solution <><><><><>
+from contextlib import suppress
+
+def aggr_operation(data: list[tuple[str, int]]) -> dict[str, int]:
+    res = {}
+    for key, value in data:
+        op, key = key[0], key[1:]
+        if key:
+            with suppress(ZeroDivisionError):
+                if n := value if op == '=' else eval(f'{res.pop(key, 0)}{op}{value}'):
+                    res[key] = n
+    return res
+
+
 # <><><><><> Best "Speedy" Solution <><><><><>
-# <><><><><> Best "3rd party" Solution <><><><><>
+def aggr_operation(data: list[tuple[str, int]]) -> dict[str, int]:
+    dic = {i[0][1:]:0 for i in data}
+    for i in [d for d in data if len(d[0]) >= 2 and (d[0][0],d[1])!=("/",0)]:
+        dic[i[0][1:]] = eval(str(dic[i[0][1:]]) + i[0][0] + str(i[1]) if i[0][0] != "=" else str(i[1]))
+    return {i:dic[i] for i in dic if dic[i]}
+
+
 # <><><><><> Uncategorized <><><><><>
+def aggr_operation(data: list[tuple[str, int]]) -> dict[str, int]:
+
+    res = {}
+    try:
+        for op, value in data:
+            var = op[1:]
+            if op[0] == '+':
+                res[var] = res.get(var, 0) + value
+            elif op[0] == '-':
+                res[var] = res.get(var, 0) - value
+            elif op[0] == '*':
+                res[var] = res.get(var, 0) * value
+            elif op[0] == '=':
+                res[var] = value
+            elif op[0] == '/':
+                if value != 0:
+                    res[var] = res.get(var, 1) / value
+                
+    except IndexError:
+        return {}
+
+    result = {k: v for k, v in res.items() if k and v}
+
+    return result
+
+
+
 # ___________________________________________________________________________________
